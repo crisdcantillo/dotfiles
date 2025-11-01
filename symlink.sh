7b4d24cbf2c41b3b6ea724a5f393465fdb01cab4
Cristhian Cantillo <crisdcantillo@gmail.com>
2025-11-01 09:13:59 -0500

    Added fonts and improved symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -1,12 +1,15 @@
 USER=crisdcantillo
+DOTSFOLDER=/home/$USER/dotfiles
 
-sudo apt update
-sudo apt upgrade
+sudo apt update && sudo apt upgrade
+
+# essential installs
+sudo apt install build-essential git tig meld curl wget unzip zip tar htop vim ripgrep fzf bat xclip ripgrep
 
 # keyd
 sudo apt install keyd
 mkdir -p /etc/keyd
-ln -sf /home/$USER/dotfiles/default.conf /etc/keyd/default.conf
+ln -sf $DOTSFOLDER/default.conf /etc/keyd/default.conf
 sudo systemctl enable keyd
 sudo systemctl start keyd
 
@@ -15,11 +18,21 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 sudo rm -rf /opt/nvim-linux-x86_64
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm nvim-linux-x86_64.tar.gz
-
 ln -sf /home/$USER/dotfiles/nvim /home/$USER/.config/nvim
-
-sudo apt install xclip		# clipboard provider
-sudo apt install ripgrep	# for text search, needed for telescope
 
 # bashrc
 ln -sf /home/$USER/dotfiles/.bash_profile /home/$USER/.bash_profile
+ln -sf $DOTSFOLDER/nvim /home/$USER/.config/nvim
+
+# nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install --lts
+
+# fonts
+mkdir -p /home/$USER/.local/share/fonts
+ln -sf $DOTSFOLDER/fonts /home/$USER/.local/share/fonts
+fc-cache -fv
+
+# bashrc
+ln -sf $DOTSFOLDER/.bash_profile /home/$USER/.bash_profile
