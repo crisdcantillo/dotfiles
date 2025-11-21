d987db6a114443db7680f6870cdaa8672393a8dc
Cristhian Cantillo <crisdcantillo@gmail.com>
2025-11-21 11:06:54 -0500

    Removed nvim from script and bashrc editing

--- symlink.sh
+++ symlink.sh
@@ -14,14 +14,6 @@ sudo systemctl enable keyd
 sudo systemctl start keyd
 sudo rm -rf $DOTSFOLDER/keyd
 
-# nvim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-sudo rm -rf /home/$USER/.config/nvim
-sudo rm -rf /opt/nvim-linux-x86_64
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-rm nvim-linux-x86_64.tar.gz
-ln -sf $DOTSFOLDER/nvim /home/$USER/.config/nvim
-
 # nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
@@ -35,10 +27,6 @@ fc-cache -fv
 # gitconfig
 sudo rm /home/$USER/.gitconfig
 ln -sf $DOTSFOLDER/.gitconfig /home/$USER/.gitconfig
-
-sudo echo 'export PATH=$PATH:/opt/nvim-linux-x86_64/bin' >> /home/$USER/.bashrc
-sudo echo 'export EDITOR=nvim' >> /home/$USER/.bashrc
-sudo echo 'export VISUAL=nvim' >> /home/$USER/.bashrc
 
 sudo apt update && sudo apt upgrade
 sudo apt autoremove
