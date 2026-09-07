0f61b0794f9d913d2836249c3497d301d3c92f54
Cristhian Cantillo <crisdcantillo@gmail.com>
2026-09-06 20:48:46 -0500

    Removed keyd, updated .gitconfig and removed packages for Debian

--- symlink.sh
+++ symlink.sh
@@ -1,19 +1,6 @@
 USER=crisdcantillo
 DOTSFOLDER=/home/$USER/dotfiles
 
-# essential installs
-sudo apt install build-essential git curl wget unzip zip tar htop vim ripgrep fzf bat xclip
-
-# keyd
-sudo rm /etc/keyd/default.conf
-git clone https://github.com/rvaiya/keyd.git
-cd keyd
-make && make install
-ln -sf $DOTSFOLDER/default.conf /etc/keyd/default.conf
-sudo systemctl enable keyd
-sudo systemctl start keyd
-sudo rm -rf $DOTSFOLDER/keyd
-
 # fonts
 mkdir -p /home/$USER/.local/share/fonts
 ln -sf $DOTSFOLDER/fonts /home/$USER/.local/share/fonts
@@ -27,5 +14,6 @@ ln -sf $DOTSFOLDER/.gitconfig /home/$USER/.gitconfig
 sudo rm /home/$USER/.tmux.conf
 ln -sf $DOTSFOLDER/.tmux.conf /home/$USER/.tmux.conf
 
-sudo apt update && sudo apt upgrade
-sudo apt autoremove
+# nvim
+sudo rm -rf /home/$USER/.config/nvim
+ln -sf $DOTSFOLDER/nvim /home/$USER/.config/nvim
